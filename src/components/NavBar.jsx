b78b9daf55fad27ac7f1a87d831482b535e51145
Isaac Rojas <isaacrojassosa@gmail.com>
2024-07-23 23:07:20 -0600

    Home and form advance

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,24 +7,24 @@ import Image from 'next/image'
 export default function NavBar() {
   return (
  
-      <nav class={styles.navbar}>
-      <div class={styles.navbar_brand}>
+      <nav className={styles.navbar}>
+      <div className={styles.navbar_brand}>
         <a href="/">OPTIFOOD</a>
       </div>
       <ul className={styles.navbar_nav}>
-        <li class={styles.nav_item}>
+        <li className={styles.nav_item}>
           <Link href="/pantry">Pantry</Link>
         </li>
-        <li class={styles.nav_item}>
+        <li className={styles.nav_item}>
           <Link href="/waste_prevented">Waste Monitor</Link>
         </li>
-        <li class={styles.nav_item}>
+        <li className={styles.nav_item}>
           <Link href="/recipes">Recipes</Link>
         </li>
-        <li class={styles.nav_item}>
+        <li className={styles.nav_item}>
           <Link href="/recipe_generator">Recipe Generator</Link>
         </li>
-        <li class={styles.nav_item}>
+        <li className={styles.nav_item}>
             <div className={styles.LogoContainer}>
                 <Image src="/pfp.webp" alt="logo" width={65} height={65} />
             </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,21 +1,187 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from '@/styles/Register.module.css'
+"use client"; // Añade esta línea al inicio del archivo
+import React, { useState } from 'react';
+import Image from 'next/image';
+import styles from '@/styles/Register.module.css';
 
 function Register() {
-  return (
-    <div className={styles.container}>
-    <div className={styles.background}></div>
-    <div className={styles.content}>
-    
-        <h1 className={styles.title}>WELCOME TO <br />OPTIFOOD</h1>
-        <h2 className={styles.sub}>Where every ingredient counts</h2>
-        <button className={styles.button}>
-          <span>LETS GET STARTED</span>
-        </button>
-    </div>
-</div>
-  )
+    const [showSignUp, setShowSignUp] = useState(false);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [selectedAllergies, setSelectedAllergies] = useState([]);
+    const [selectedCookingEquipment, setSelectedCookingEquipment] = useState([]);
+    const[selectedDiet, setSelectedDiet] = useState([]);
+    const[selectedPreferences, setSelectedPreferences] = useState([]);
+    const [showAllergiesDropdown, setShowAllergiesDropdown] = useState(false);
+    const [showDietDropdown, setShowDietDropdown] = useState(false);
+    const [showPreferencesDropdown, setShowPreferencesDropdown] = useState(false);
+    const [showEquipmentDropdown, setShowEquipmentDropdown] = useState(false);
+
+
+    const handleButtonClick = () => {
+        setShowSignUp(true);
+    };
+
+    const handleSubmitBoton = () => {
+        console.log(selectedAllergies);
+    }
+
+    const handleSelectAllergy = (allergy) => {
+        if (selectedAllergies.includes(allergy)) {
+            setSelectedAllergies(selectedAllergies.filter(a => a !== allergy));
+        } else {
+            setSelectedAllergies([...selectedAllergies, allergy]);
+        }
+    };
+
+    const handleSelectCookingEquipment = (equipment) => {
+        if (selectedCookingEquipment.includes(equipment)) {
+            setSelectedCookingEquipment(selectedCookingEquipment.filter(e => e !== equipment));
+        } else {
+            setSelectedCookingEquipment([...selectedCookingEquipment, equipment]);
+        }
+    }
+
+    const handleSelectDiet = (diet) => {
+        if (selectedDiet.includes(diet)) {
+            setSelectedDiet(selectedDiet.filter(d => d !== diet));
+        } else {
+            setSelectedDiet([...selectedDiet, diet]);
+        }
+    }
+
+    const handleSelectPreferences = (preference) => {
+        if (selectedPreferences.includes(preference)) {
+            setSelectedPreferences(selectedPreferences.filter(p => p !== preference));
+        } else {
+            setSelectedPreferences([...selectedPreferences, preference]);
+        }
+    }
+
+    const toggleAllergiesDropdown = () => {
+        setShowAllergiesDropdown(!showAllergiesDropdown);
+        // Opcionalmente, cierra el otro menú desplegable cuando uno se abre
+        setShowDietDropdown(false);
+        setShowPreferencesDropdown(false);
+    };
+
+    const toggleDietDropdown = () => {
+        setShowDietDropdown(!showDietDropdown);
+        // Opcionalmente, cierra el otro menú desplegable cuando uno se abre
+        setShowAllergiesDropdown(false);
+        setShowPreferencesDropdown(false);
+    };
+
+    const togglePreferencesDropdown = () => {
+        setShowPreferencesDropdown(!showPreferencesDropdown);
+        // Opcionalmente, cierra el otro menú desplegable cuando uno se abre
+        setShowDietDropdown(false);
+        setShowAllergiesDropdown(false);
+    }
+
+    const toggleEquipmentDropdown = () => {
+        setShowEquipmentDropdown(!showEquipmentDropdown);
+        // Opcionalmente, cierra el otro menú desplegable cuando uno se abre
+        setShowDietDropdown(false);
+        setShowAllergiesDropdown(false);
+        setShowPreferencesDropdown(false);
+    }
+
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.background}></div>
+            {!showSignUp ? (
+                <div className={styles.content}>
+                    <h1 className={styles.title}>WELCOME TO <br />OPTIFOOD</h1>
+                    <h2 className={styles.sub}>Where every ingredient counts</h2>
+                    <button className={styles.button} onClick={handleButtonClick}>
+                        <span>LETS GET STARTED</span>
+                    </button>
+                </div>
+            ) : (
+                <div className={styles.content}>
+                    <h1 className={styles.title2}>FILL OUT THE FORM :] </h1>
+                    <div className={styles.LogoContainer}>
+                        <Image src="/pfp.webp" alt="logo" width={125} height={125} />
+                    </div>
+
+                    <div className={styles.preferences}>
+                            <label onClick={() => toggleAllergiesDropdown(!showDropdown)}>Allergies</label>
+                            {showAllergiesDropdown && (
+                                <div >
+                                    {['Nuts', 'Gluten', 'Dairy', 'Shellfish'].map((allergy, index, array) => (
+                                        <div
+                                            key={allergy}
+                                            className={styles.optionss}
+                                            onClick={() => handleSelectAllergy(allergy)}
+                                        >
+                                            <div className={styles.checkbox}
+                                                style={{   
+                                                    backgroundColor: selectedAllergies.includes(allergy) ? '#592941' : 'transparent',
+                                                }}
+                                            ></div>
+                                            {allergy}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        
+                    </div>
+
+                    <div className={styles.preferences}>
+                            <label onClick={() => toggleDietDropdown()}>Diet</label>
+                            {showDietDropdown && (
+                                <div >
+                                    {['Vegan', 'Vegetarian', 'Low-Carb'].map((diet, index, array) => (
+                                        <div
+                                            key={diet}
+                                            className={styles.optionss}
+                                            onClick={() => handleSelectDiet(diet)}
+                                        >
+                                            <div className={styles.checkbox}
+                                                style={{   
+                                                    backgroundColor: selectedDiet.includes(diet) ? '#592941' : 'transparent',
+                                                }}
+                                            ></div>
+                                            {diet}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        
+                    </div>
+
+                    <div className={styles.preferences}>
+                            <label onClick={() => togglePreferencesDropdown()}>Preffered Cuisines </label>
+                            {showPreferencesDropdown && (
+                                <div >
+                                    {['Mexican', 'Italian', 'Chinese', 'Japanese', 'Thai', 'Greek', 'Cuban', 'Indian', 'Vietnamese'].map((preference, index, array) => (
+                                        <div
+                                            key={preference}
+                                            className={styles.optionss}
+                                            onClick={() => handleSelectPreferences(preference)}
+                                        >
+                                            <div className={styles.checkbox}
+                                                style={{   
+                                                    backgroundColor: selectedPreferences.includes(preference) ? '#592941' : 'transparent',
+                                                }}
+                                            ></div>
+                                            {preference}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        
+                    </div>
+
+
+                    <button onClick={handleSubmitBoton} type="submit">Submit</button>
+           
+                   
+                </div>
+                
+            )}
+        </div>
+    );
 }
 
-export default Register
+export default Register;
